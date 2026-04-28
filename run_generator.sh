1432a880a9a3c3002d0b78d8a6ef6fc82d8e13f4
mirkotp <mirkox90@gmail.com>
2026-04-28 12:34:51 +0200

    fix

---
+++ run_generator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source venv/bin/activate
+PYTHONPATH=src streamlit run src/idb1/generator_gui.py
---
+++ run_reader.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source venv/bin/activate
+PYTHONPATH=src streamlit run src/idb1/reader_gui.py
